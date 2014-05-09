2d4a84cb147ef7b5fc40b42f2e913cb6e788e0b3
Wayne Simmerson <wsimmerson@gmail.com>
2014-05-09 16:27:36 -0400

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @title = 'Home'
+  end
+      
+  def contact
+    @title = 'Contact'
   end
 
-  def contact
+  def about
+    @title = 'About'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
+  #####
+  # Pages Controller Routes
   get 'pages/home'
-
   get 'pages/contact'
+  get 'pages/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

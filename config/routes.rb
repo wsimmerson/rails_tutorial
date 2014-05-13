8217c9a6f8899b1afb49d199cddec6cd8f8e8b2b
Wayne Simmerson <wsimmerson@gmail.com>
2014-05-13 17:05:01 -0400

    up to sectoin 9.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   
   #####
   # Pages Controller Routes
@@ -8,6 +9,11 @@ Rails.application.routes.draw do
   get '/contact', :to => 'pages#contact'
 
   get '/signup', :to => 'users#new'
+
+  ######
+  ## Sessions controller Routes
+  get '/signin',  :to => "sessions#new"
+  get '/signout', :to => "sessions#destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

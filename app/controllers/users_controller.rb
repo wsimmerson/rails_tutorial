089c19f37387e701b71a7a582ebc324ad80aaabe
Wayne Simmerson <wsimmerson@gmail.com>
2014-05-13 15:32:03 -0400

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,28 @@
 class UsersController < ApplicationController
   def new
   @title = "Sign up"
+  @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
     @title = @user.name
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -103,7 +103,7 @@ describe User do
           wrong_password_user.should be_nil
         end
         it "should return nil for an email address with no user" do
-          nonexistent_user = User.authentiate("bar@foo.com", @attr[:password])
+          nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
           nonexistent_user.should be_nil
         end
         it "should return the user on email/password match" do
